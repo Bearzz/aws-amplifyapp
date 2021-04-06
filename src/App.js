04c1b038dfa091f35b2083283d8c66a2783bff93
Stephen Bearnson <sbearnson@gmail.com>
2021-04-06 09:17:47 -0600

    Removed B

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Steve B
+          Learn React with Steve
         </a>
        </header>
     </div>
